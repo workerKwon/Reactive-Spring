8dc9ab1f1643643560d0b74317a5b41d76ab2e91
workerKwon <dmdmdm1322@gmail.com>
2022-06-02 17:38:32 +0900

    Update RxJava를 이용한 온도 탐지 이벤트 반응형 애플리케이션 개발

---
+++ src/main/java/com/spring/reactive/rx_java_temperature/RxTemperature.java
@@ -0,0 +1,10 @@
+package com.spring.reactive.rx_java_temperature;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+final class RxTemperature {
+    private final double value;
+}
